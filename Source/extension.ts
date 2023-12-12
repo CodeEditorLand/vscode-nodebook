ce07296ba9b845dc6a74e3b34ff0128cb606f62b
Nikola Hristov <nikola@nikolahristov.tech>
2023-12-13 01:16:40 +0200

    squash!

--- Source/extension.ts
+++ Source/extension.ts
@@ -3,36 +3,31 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import * as vscode from "vscode";
-import { NodebookContentProvider } from "./nodebookProvider";
+import * as vscode from 'vscode';
+import { NodebookContentProvider } from './nodebookProvider';
 
 export function activate(context: vscode.ExtensionContext) {
+
 	const nodebookContentProvider = new NodebookContentProvider();
 
 	context.subscriptions.push(
-		vscode.notebook.registerNotebookContentProvider(
-			"nodebook",
-			nodebookContentProvider
-		),
 
-		vscode.commands.registerCommand("nodebook.toggleDebugging", () => {
+		vscode.notebook.registerNotebookContentProvider('nodebook', nodebookContentProvider),
+
+		vscode.commands.registerCommand('nodebook.toggleDebugging', () => {
 			if (vscode.window.activeNotebookEditor) {
 				const { document } = vscode.window.activeNotebookEditor;
-				const nodebook = nodebookContentProvider.lookupNodebook(
-					document.uri
-				);
+				const nodebook = nodebookContentProvider.lookupNodebook(document.uri);
 				if (nodebook) {
 					nodebook.toggleDebugging(document);
 				}
 			}
 		}),
 
-		vscode.commands.registerCommand("nodebook.restartKernel", () => {
+		vscode.commands.registerCommand('nodebook.restartKernel', () => {
 			if (vscode.window.activeNotebookEditor) {
 				const { document } = vscode.window.activeNotebookEditor;
-				const nodebook = nodebookContentProvider.lookupNodebook(
-					document.uri
-				);
+				const nodebook = nodebookContentProvider.lookupNodebook(document.uri);
 				if (nodebook) {
 					nodebook.restartKernel();
 				}
@@ -41,4 +36,5 @@ export function activate(context: vscode.ExtensionContext) {
 	);
 }
 
-export function deactivate() {}
+export function deactivate() {
+}
